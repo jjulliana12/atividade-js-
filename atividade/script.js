b3aca89e516d9c265ea95eba15a45ae0b432071b
jjulliana12 <126627608+jjulliana12@users.noreply.github.com>
2024-05-10 12:26:55 -0300

    Add files via upload

---
+++ atividade/script.js
@@ -0,0 +1,47 @@
+//Mostre um alerta com a mensagem "Bem-vindo ao nosso site!".
+
+alert("Bem-vindo ao nosso site:)");
+
+//Declare uma variável chamada nome e atribua a ela o valor "Lua".
+
+var nome = "Lua";
+console.log (nome)
+
+//Crie uma variável chamada idade e atribua a ela o valor 25.
+
+var idade = 25;
+console.log (idade)
+
+//Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+
+var numeroDeVendas = 50;
+console.log (numeroDeVendas)
+
+//Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+
+var saldoDisponivel = 1000;
+console.log (saldoDisponivel)
+
+//Exiba um alerta com o texto "Erro! Preencha todos os campos."
+
+alert ("ERRO! Preencha todos os campos")
+
+//Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos." Agora exiba um alerta com o valor da variável mensagemDeErro.
+
+let mensagemDeErro = prompt ("Preencha todos os campos ")
+
+//Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+
+var nome = prompt ("Qual seu nome? ")
+
+//Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+
+var idade = prompt (" Qual sua idade?")
+
+//Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade. Agora, a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+
+if(idade >18 ){
+alert("Pode tirar habilitação!")
+} else {
+    alert ("Menor de idade! Não pode tirar habilitação")
+}
